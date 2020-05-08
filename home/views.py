1d9c80120b388b419fa8e07cb7394ed81be8df72
erikapereira <pereiraerika@outlook.com>
2020-05-08 18:37:23 +0100

    not much just messing about with html for contact form

--- home/views.py
+++ home/views.py
@@ -5,8 +5,12 @@ def home(request):
     return render(request, 'home.html', {})
 
 def about(request):
-    form = ContactForm()
-    return render(request, 'about.html', {'form': form})
+    # form = ContactForm()
+    # return render(request, 'about.html', {'form': form})
+
+    context = {}
+    context['form'] = ContactForm()
+    return render(request, "about.html", context)
 
 def portfolio (request):
     return render(request, 'portfolio.html', {})
